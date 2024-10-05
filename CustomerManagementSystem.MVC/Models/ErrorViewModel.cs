b8d0bb04034bb2101ae1db5fc1a659fc42acda8a
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-10-05 16:38:18 +0300

    Add ASP.NET Core MVC project

---
+++ CustomerManagementSystem.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerManagementSystem.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
